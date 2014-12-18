79d4ad30d3fc6e8e2146fbde89532cb11979ce87
MichaelDaum <github@michaeldaumconsulting.com>
2014-12-18 10:13:55 +0100

    Item12864: make sure plugins are releasable
    ... using proper VERSION and RELEASE tags

--- lib/Foswiki/Plugins/VotePlugin.pm
+++ lib/Foswiki/Plugins/VotePlugin.pm
@@ -23,7 +23,7 @@ use vars qw(
   $VERSION $RELEASE $NO_PREFS_IN_TOPIC $SHORTDESCRIPTION
 );
 
-$VERSION           = '$Rev$';
+$VERSION           = '1.33';
 $RELEASE           = '1.33';
 $NO_PREFS_IN_TOPIC = 1;
 $SHORTDESCRIPTION  = 'Simple way to count votes';
